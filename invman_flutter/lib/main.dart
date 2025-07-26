799f502b8c79a1cf8dd28406f27ec1d5315dc3ee
Baudin Valentin <valentin.baudin@hes-so.ch>
2025-07-26 08:46:13 +0200

    fix(app): avoid rotation

--- invman_flutter/lib/main.dart
+++ invman_flutter/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:invman_flutter/config/generated/l10n.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:invman_flutter/core/core.dart';
@@ -11,6 +12,10 @@ late final SharedPreferences prefs;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
 
   prefs = await SharedPreferences.getInstance();
 
